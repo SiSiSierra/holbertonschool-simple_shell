365f34d7699ad6d020be2b584739dec818d0b76a
Trevnh <trevorhuynh@hotmail.com>
2026-04-18 07:51:56 +0000

    Add shell.h with relevant header files included

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*
+ * Headers
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/*
+ *  Prototype Functions
+ */
+
+
+#endif

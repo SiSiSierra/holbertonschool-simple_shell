d67ce0661e84e75301b67eebf2fe2d08e28e2b80
Trevnh <trevorhuynh@hotmail.com>
2026-04-20 04:28:10 +0000

    Add check_arg and strcat3 to path.c and add the prototypes to shell.h

--- path.c
+++ path.c
@@ -48,16 +48,16 @@ char **get_path_directories(char *path)
 	char **path_dirs;
 	char *dir;
 	char *temp = strdup(path);
-	int idx = 0;
+	int i = 0;
 
 	dir = strtok(temp, ":");
 	while (dir != NULL)
 	{
-		idx++;
+		i++;
 		dir = strtok(NULL, ":");
 	}
 	free(temp);
-	path_dirs = malloc((idx + 1) * sizeof(*path_dirs));
+	path_dirs = malloc((i + 1) * sizeof(*path_dirs));
 	if (!path_dirs)
 	{
 		return (NULL);
@@ -66,8 +66,80 @@ char **get_path_directories(char *path)
 	for (dir = strtok(path, ":"); dir; dir = strtok(NULL, ":"))
 	{
 	path_dirs[i] = dir;
-		idx++;
+		i++;
 	}
-	path_dirs[idx] = NULL;
+	path_dirs[i] = NULL;
 	return (path_dirs);
+}
+
+/**
+ * strcat3 - concatenates 3 strings together.
+ * @a: first string
+ * @b: middle string
+ * @c: last string
+ *
+ * Return: final string with all 3 concatenated.
+ */
+char *strcat3(char *a, char *b, char *c)
+{
+	size_t size = strlen(a) + strlen(b) + strlen(c) + 1;
+	char *str = malloc(size);
+
+	strcpy(str, a);
+	strcat(str, b);
+	strcat(str, c);
+
+	return (str);
+}
+
+/**
+ * check_arg - Returns the full name of given argument
+ * @args: array of arguments to run
+ * @path_directories: array of all PATH directories
+ * @flag: flag to change based on args[0]
+ *
+ * Return: full argument name if found, NULL otherwise.
+ */
+char *check_arg(char **args, char **path_directories, int *flag)
+{
+	char *temp = NULL;
+	int i = 0;
+	struct stat test;
+
+	if (args == NULL)
+	{
+		return (NULL);
+	}
+	if (*args[0] == '/' || *args[0] == '.')
+	{
+		if (stat(args[0], &test) == 0)
+		{
+			*flag = 0;
+			return (args[0]);
+		}
+		else
+			return (NULL);
+	}
+	else
+	{
+		while (path_directories != NULL && path_directories[i])
+		{
+			printf("test\n");
+			temp = strcat3(path_directories[i], "/", args[0]);
+			printf("temp = %s\n", temp);
+			if (stat(temp, &test) == 0)
+			{
+				*flag = 1;
+				return (temp);
+			}
+			else
+			{
+				i++;
+				free(temp);
+			}
+		}
+		temp = NULL;
+	}
+	*flag = -1;
+	return (temp);
 }
